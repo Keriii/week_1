f2c23de7b00ecd90a5e7db1af2d86ea90e0a3614
Kerod Sisay <Kerod53@gmail.com>
2023-12-15 01:51:15 +0300

    functions to upload data

--- script/data_extraction.py
+++ script/data_extraction.py
@@ -2,32 +2,83 @@ import pandas as pd
 from sqlalchemy import create_engine
 import psycopg2
 
-def data_extraction():
-    engine = create_engine('postgresql+psycopg2://postgres:kerod53@@localhost:5432/Telecom')
-    return engine
+class pgdatabase:
+    def __init__(self):
+        pass
+    
+    
+    my_parameters = {'dbname': 'Telecom',
+                    'user': 'postgres',
+                    'password':'kerod53@',
+                    'host':'localhost',
+                    'port': '5432'}
+    
+    def connection(self, parameters: dict = my_parameters):
+        """
+        Connects to the PostgreSQL database. for a given paramters:
+        connection_params is a dictionary that define the following:
+        {
+            'dbname': 'your_database_name',
+            'user': 'your_username',
+            'password': 'your_password',
+            'host': 'your_host',
+            'port': 'your_port'
+            }
+        """
+        try:
+            conn = psycopg2.connect(**parameters)
+            return conn
+        except psycopg2.Error as e:
+            print(f"Error: Unable to connect to the database. {e}")
+            return None
 
-def get_dataframe(engine, table_name):
-    query = f'SELECT * FROM public.{table_name}'
-    data = pd.read_sql_query(query, engine)
-    return data
+    def get_dataframe_sql(self, conn, table_name):
+        query = f'SELECT * FROM public.{table_name}'
+        data = pd.read_sql_query(query, conn)
+        conn.close()
+        return data
+    
+    def create_engine(self, connection_params: dict = my_parameters):
+        """
+        creates engine using sqlalchemy for a given paramters:
+        """
+        engine = create_engine("postgresql://postgres:kerod53@@localhost:5432/Telecom")
+        return engine
 
-def connection():
-    conn = psycopg2.connect(dbname = 'Telecom',
-                            user = 'postgres',
-                            password = 'kerod53@',
-                            host = 'localhost',
-                            port = '5432')
-    return conn
+    def write_dataframe_to_table(self, df: pd.DataFrame, table_name: str,engine)->None:
+        """
+        Writes a pandas dataframe to a new table in the PostgreSQL database.
+        """
+        df.to_sql(table_name, engine, index=False, if_exists='replace')
+        print(f"Dataframe successfully written to the '{table_name}' table.")
 
-def get_dataframe_sql(conn, table_name):
-    query = f'SELECT * FROM public.{table_name}'
-    data = pd.read_sql_query(query, conn)
-    return data
+    def update_table_by_appending(df, table_name, connection_params = my_parameters):
+        """
+        Appends a pandas dataframe to an existing PostgreSQL table.
+        """
+        engine = create_engine(f"postgresql://{connection_params['user']}:{connection_params['password']}@{connection_params['host']}:{connection_params['port']}/{connection_params['dbname']}")
+        df.to_sql(table_name, engine, index=False, if_exists='append')
+        print(f"Dataframe successfully appended to the '{table_name}' table.")
 
-def main():
-    engine = data_extraction()
-    data = get_dataframe(engine, 'data')
-    print(data.head())
+    def delete_table(table_name, connection_params = my_parameters):
+        """
+        Deletes a table from the PostgreSQL database.
+        """
+        connection = connect_to_database(connection_params)
+        if connection:
+            cursor = connection.cursor()
+            cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
+            connection.commit()
+            connection.close()
+            print(f"Table '{table_name}' successfully deleted.")
+        else:
+            print("Error: Unable to connect to the database.")
 
-if __name__ == '__main__':
-    main()
+    def main():
+        conn = connection()
+        data = get_dataframe_sql(conn, 'telco_churn')
+        print(data.head())
+        conn.close()
+
+    if __name__ == '__main__':
+        main()
